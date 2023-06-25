bedb4473e6996bb4d1d58036bc3d7d2fd5ab6e7b
Kampanman <it.1231.newsoul@gmail.com>
2023-06-25 19:05:45 +0900

    first commit

---
+++ static/js/VJCP_noteImput.js
@@ -0,0 +1,24 @@
+/**
+ * コンポーネント：ノート入力エリア
+ */
+let noteImput = Vue.component("note-input", {
+  template: `<div class="areaParts">
+
+  </div>`,
+  data: function(){
+    return {
+      // 
+    }
+  },
+  created: function () {
+    this.init();
+  },
+  methods: {
+    // 画面初期表示処理
+    async init() {
+      // 
+    },
+  },
+});
+
+export default noteImput;
